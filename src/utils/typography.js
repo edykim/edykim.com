3eea6b29889350f96a0428f476d1789986ac62af
Edward Kim <edward@edykim.com>
2018-09-27 11:47:37 -0700

    Update style for head

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -8,22 +8,27 @@ theme.bodyFontFamily.splice(theme.bodyFontFamily.length - 1, 0, 'Jeju Myeongjo')
 
 const proxy = theme.overrideStyles;
 theme.overrideStyles = (data, rhythm) => {
-    const result = proxy(data, rhythm);
-    return {
-        ...result,
-        ul: {
-            listStyle: 'square',
-        },
-        li: {
-            marginBottom: 'initial',
-        },
-        'li p': {
-            margin: '0',
-        },
-        'ul ul': {
-            marginTop: '0'
-        }
+  const result = proxy(data, rhythm);
+  return {
+    ...result,
+    ul: {
+      listStyle: 'square',
+      paddingLeft: '1.5rem',
+    },
+    li: {
+      marginBottom: 'initial',
+    },
+    'li p': {
+      margin: '0',
+    },
+    'ul ul': {
+      marginTop: '0',
+      paddingLeft: '0',
+    },
+    'h1 + h2, h2 + h3, h3 + h4, h4 + h5': {
+      marginTop: '1rem',
     }
+  }
 }
 
 const typography = new Typography(theme);
