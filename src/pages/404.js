8f8891ef936a3565d9fea031c3dd60951708c8a0
Edward Kim <edward@edykim.com>
2018-09-27 09:53:26 -0700

    Add title on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Helmet from 'react-helmet'
 
 const NotFoundPage = () => (
   <Layout>
+    <Helmet title="404 not found"></Helmet>
     <h1>NOT FOUND</h1>
     <p>You just hit a page that doesn&#39;t exist...</p>
 
