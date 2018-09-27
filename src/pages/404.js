384cec0c560525a333a17b5474bcd5110a76e2dd
Edward Kim <edward@edykim.com>
2018-09-27 09:50:49 -0700

    Put the form back on 404 error
    Cause the link issue is from gatsby.

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,6 +5,15 @@ const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a page that doesn&#39;t exist...</p>
+
+    <p>
+      What did you looking for? Let's google it!
+    </p>
+
+    <form action="http://www.google.com/search" method="get">
+      <input type="text" name="q" />
+      <input type="submit" value="search" />
+    </form>
   </Layout>
 )
 
