9ee1c44365f9df964017958dc3285a04efc954d6
Edward Kim <edward@edykim.com>
2018-10-07 14:27:53 -0700

    Relocate the code for gernerating redirect.json

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,6 @@
  */
 
 const path = require('path');
-const urlResolve = require('url').resolve;
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -59,71 +58,3 @@ exports.createPages = ({ graphql, actions }) => {
     });
   });
 };
-
-// generate redirect file
-// const path = require('path');
-const fs = require('fs');
-const pify = require('pify');
-const mkdirp = require('mkdirp');
-const config = require('./gatsby-config');
-
-const writeFile = pify(fs.writeFile)
-const runQuery = (handler, query) =>
-  handler(query).then(r => {
-    if (r.errors) {
-      throw new Error(r.errors.join(`, `))
-    }
-
-    return r.data
-  })
-
-exports.onPostBuild = async ({ graphql }) => {
-  const query = `
-  {
-    redirects: allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            private
-            draft
-            history {
-              from
-            }
-          }
-          fields {
-            url
-          }
-        }
-      }
-    }
-  }`;
-
-  const filename = 'redirect.json';
-  const publicPath = './public';
-
-  const result = await runQuery(graphql, query);
-
-  var redirects = [];
-  result.redirects.edges.forEach(({ node }) => {
-    if (node.frontmatter.private || node.frontmatter.draft) return;
-    if (!node.frontmatter.history || node.frontmatter.history.length === 0) return;
-
-    const to = urlResolve(config.siteMetadata.siteUrl, node.fields.url);
-    const result = node.frontmatter.history.map(v => ({
-      from: v.from,
-      to
-    }));
-    
-    redirects = redirects.concat(result);
-  });
-
-  const outputPath = path.join(publicPath, filename);
-  const outputDir = path.dirname(publicPath);
-  if (!fs.existsSync(outputDir)) {
-    mkdirp.sync(outputDir);
-  }
-
-  await writeFile(outputPath, JSON.stringify(redirects, null, 2));
-  console.log(`${redirects.length} of redirections generated. (${outputPath})`)
-  return Promise.resolve();
-}
