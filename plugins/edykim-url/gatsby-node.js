84f2a970ab3848778f2e2c813992bfe748ee544d
Edward Kim <edward@edykim.com>
2018-10-07 14:13:38 -0700

    Update url handler with more options

--- plugins/edykim-url/gatsby-node.js
+++ plugins/edykim-url/gatsby-node.js
@@ -1,28 +1,30 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const { defaultOptions } = require(`./internals`)
 
-const isInTargetResources = (node, targetTypes) => {
+const isInTargetResources = (node, { targetTypes }) => {
   return targetTypes.indexOf(node.internal.type) !== -1
 }
 
 // Create slug based on file path so that posts keep path based route and
 // also maintains configurable urls.
-const createSlugField = ({node, getNode, createNodeField}) => {
+const createSlugField = ({ node, getNode, createNodeField }, { slugFieldName }) => {
   const slug = createFilePath({ node, getNode, basePath: `pages` })
 
   createNodeField({
     node,
-    name: `slug`,
+    name: slugFieldName,
     value: slug
   })
 }
 
 // Create url based on generating rules of the website
-const createUrlField = ({ node, createNodeField }, urlHandler) => {
-  const url = urlHandler(node)
+const createUrlField = ({ node, createNodeField }, options) => {
+  const { urlHandler, urlFieldName } = options
+  const url = urlHandler({ node, ...options })
+
   createNodeField({
     node,
-    name: `url`,
+    name: urlFieldName,
     value: url,
   })
 }
@@ -35,8 +37,8 @@ exports.onCreateNode = ({ node, getNode, actions }, pluginOptions) => {
     ...pluginOptions,
   }
 
-  if (isInTargetResources(node, options.targetTypes)) {
-    createSlugField({node, getNode, createNodeField})
-    createUrlField({node, createNodeField}, options.urlHandler)
+  if (isInTargetResources(node, options)) {
+    createSlugField({node, getNode, createNodeField}, options)
+    createUrlField({node, createNodeField}, options)
   }
 };
