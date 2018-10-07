c7f970392911f7776d4a67b8f63cb06357e0db98
Edward Kim <edward@edykim.com>
2018-10-07 14:27:53 -0700

    Relocate the code for gernerating redirect.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,5 +75,6 @@ module.exports = {
     },
 
     `edykim-url`,
+    `edykim-redirect-json`,
   ],
 }
---
+++ plugins/edykim-redirect-json/gatsby-node.js
@@ -0,0 +1,70 @@
+const path = require(`path`)
+const urlResolve = require(`url`).resolve
+
+const fs = require(`fs`)
+const pify = require(`pify`)
+const mkdirp = require(`mkdirp`)
+const config = require(`./../../gatsby-config`)
+
+const writeFile = pify(fs.writeFile)
+
+const runQuery = (handler, query) =>
+  handler(query).then(r => {
+    if (r.errors) {
+      throw new Error(r.errors.join(`, `))
+    }
+
+    return r.data
+  })
+
+exports.onPostBuild = async ({ graphql }) => {
+  const query = `
+  {
+    redirects: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            private
+            draft
+            history {
+              from
+            }
+          }
+          fields {
+            url
+          }
+        }
+      }
+    }
+  }`
+
+  const filename = `redirect.json`
+  const publicPath = `./public`
+
+  const result = await runQuery(graphql, query)
+
+  var redirects = []
+  result.redirects.edges.forEach(({ node }) => {
+    if (node.frontmatter.private || node.frontmatter.draft) return
+    if (!node.frontmatter.history || node.frontmatter.history.length === 0) return
+
+    const to = urlResolve(config.siteMetadata.siteUrl, node.fields.url)
+    const result = node.frontmatter.history.map(v => ({
+      from: v.from,
+      to
+    }))
+    
+    redirects = redirects.concat(result)
+  })
+
+  const outputPath = path.join(publicPath, filename)
+  const outputDir = path.dirname(publicPath)
+  if (!fs.existsSync(outputDir)) {
+    mkdirp.sync(outputDir)
+  }
+
+  await writeFile(outputPath, JSON.stringify(redirects, null, 2))
+
+  console.log(`${redirects.length} of redirections generated. (${outputPath})`)
+  return Promise.resolve()
+}
