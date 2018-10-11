56087991db87b1c5e8aef97dc5652a7bed6df5b0
Edward Kim <edward@edykim.com>
2018-10-11 11:22:07 -0700

    Update social card

--- src/templates/taxonomy.js
+++ src/templates/taxonomy.js
@@ -8,11 +8,23 @@ class TaxonomyTemplate extends React.Component {
   render() {
     const { articles } = this.props.data
     const taxonomy = this.props.pageContext.taxonomy
+    const path = this.props.pageResources.page.path
     const lang = this.props.pageContext.lang
+
+    const title = `Archives: ${taxonomy}`
+
     return (
       <Layout>
-        <Helmet title={`Archives: ${taxonomy}`}>
+        <Helmet title={title}>
           <meta name="robots" content="noindex" />
+          <meta property="og:locale" content={lang === 'ko' ? 'ko_KR' : 'en_US'} />
+          <meta property="og:type" content="object" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content={`https://edykim.com${path}`} />
+          <meta property="og:site_name" content="Here, Edward" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:site" content={lang === `ko` ? `@haruair` : `@heyedykim`} />
         </Helmet>
 
         <h1>{taxonomy}</h1>
