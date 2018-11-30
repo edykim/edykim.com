850e10401629be897f00ad0f1881c5e966509630
Edward Yong Gyun Kim <edward@edykim.com>
2018-11-30 00:01:45 -0800

    Update title link

--- src/components/Frontispiece.js
+++ src/components/Frontispiece.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Headline from './Headline'
 import './Frontispiece.css'
 
-const Frontispiece = ({ frontmatter }) => (
+const Frontispiece = ({ frontmatter, url }) => (
   <div className={`frontispiece`}>
-    <h1>{frontmatter.title}</h1>
+      <h1>
+        { url
+          ? <Link to={url}>{frontmatter.title}</Link>
+          : frontmatter.title }
+      </h1>
+
     <Headline line={frontmatter.headline || []} />
   </div>
 )
