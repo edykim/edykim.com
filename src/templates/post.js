8c232fb826b7fb34d166ed79161eb9a18a69e54d
Edward Yong Gyun Kim <edward@edykim.com>
2018-11-30 00:01:45 -0800

    Update title link

--- src/templates/post.js
+++ src/templates/post.js
@@ -14,7 +14,7 @@ export default ({ data }) => {
     <Layout lang={post.frontmatter.lang}>
       <Card info={post} />
 
-      <Frontispiece frontmatter={post.frontmatter} />
+      <Frontispiece frontmatter={post.frontmatter} url={post.fields.url} />
 
       <div
         className="content"
