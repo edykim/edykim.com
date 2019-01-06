9f876282d395ff509b8822db691918fb6384e39b
Edward Yong Gyun Kim <edward@edykim.com>
2019-01-06 13:11:28 -0800

    Update config for desktop PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'edykim.com',
+        short_name: 'edykim',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
