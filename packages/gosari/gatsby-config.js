abe5f15d521d80e4789e5eef5b0efe28b5be12d5
Edward Yong Gyun Kim <edward@edykim.com>
2019-03-10 10:00:41 -0700

    Remove offline support

--- packages/gosari/gatsby-config.js
+++ packages/gosari/gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `@edykim/gatsby-plugin-url`,
