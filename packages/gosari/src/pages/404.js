c9c085bd8e375db7edd6c71634e3ac4ae599abb9
Edward Kim <33057457+edykim@users.noreply.github.com>
2019-03-13 03:38:00 -0700

    Update 404 page (#17)

--- packages/gosari/src/pages/404.js
+++ packages/gosari/src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,8 +12,17 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>404 Not Found</h1>
+        <p>요청한 페이지가 웹사이트에 존재하지 않습니다.</p>
+
+        <ul>
+          <li>
+            <Link to={`/`}>첫 페이지로 이동</Link>
+          </li>
+          <li>
+            <Link to={`/`}>글 전체 목록으로 이동</Link>
+          </li>
+        </ul>
       </Layout>
     )
   }
