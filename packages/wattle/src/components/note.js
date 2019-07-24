083c370ea5045098c3a3304a23feb0c3ce425ee5
Edward Yong Gyun Kim <edward@edykim.com>
2019-07-24 15:15:19 -0700

    Update note style

---
+++ packages/wattle/src/components/note.js
@@ -0,0 +1,105 @@
+import React from "react"
+import styled from "styled-components"
+
+const Section = styled.div`
+  font-size: 18px;
+  line-height: 1.7;
+  font-weight: 400;
+
+  a[href^="http"]:after {
+    content: "external";
+    color: #333333;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 0.6em;
+    margin-left: 5px;
+    vertical-align: super;
+  }
+
+  ul p {
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 1.5em;
+    font-weight: 700;
+  }
+  h2 {
+    font-size: 1.3em;
+    font-weight: 700;
+  }
+  h3 {
+    font-size: 1.1em;
+    font-weight: 700;
+  }
+  h4,
+  h5 {
+    font-size: 1em;
+  }
+  h1 {
+    counter-increment: h1counter;
+    counter-reset: h2counter;
+  }
+  h2 {
+    counter-increment: h2counter;
+    counter-reset: h3counter;
+  }
+  h3 {
+    counter-increment: h3counter;
+    counter-reset: h4counter;
+  }
+  h4 {
+    counter-increment: h4counter;
+    counter-reset: h5counter;
+  }
+  h2:before {
+    content: counter(h2counter) ". ";
+  }
+  h3:before {
+    content: counter(h2counter) "." counter(h3counter) ". ";
+  }
+  h4:before {
+    content: counter(h2counter) "." counter(h3counter) "." counter(h4counter)
+      ". ";
+  }
+  h1:not(.skip) :before {
+    content: counter(h1counter) ". ";
+  }
+  h1:not(.skip) ~ h2:before {
+    content: counter(h1counter) "." counter(h2counter) ". ";
+  }
+  h1:not(.skip) ~ h3:before {
+    content: counter(h1counter) "." counter(h2counter) "." counter(h3counter)
+      ". ";
+  }
+  h1:not(.skip) ~ h4:before {
+    content: counter(h1counter) "." counter(h2counter) "." counter(h3counter)
+      "." counter(h4counter) ". ";
+  }
+
+  .no-number:before {
+    content: none;
+  }
+
+  .reset-number {
+    counter-reset: h1counter 0 h2counter 0 h3counter 0 h4counter 0 h5counter 0;
+  }
+
+  hr {
+    height: 0;
+    border: 0;
+    border-bottom: 4px double #666666;
+    max-width: 20rem;
+    margin: 4rem auto 4rem;
+
+    @media (max-width: 750px) {
+      margin-top: 2rem;
+      margin-bottom: 1rem;
+      width: 200px;
+    }
+  }
+`
+
+export const Content = ({ html }) => (
+  <Section dangerouslySetInnerHTML={{ __html: html }} />
+)
