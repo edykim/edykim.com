23b8b0343f563faa8c55423db5c9cccdf8c0bd3c
Edward Yong Gyun Kim <edward@edykim.com>
2019-07-24 10:33:48 -0700

    Add inner link plugin

--- packages/wattle/gatsby-config.js
+++ packages/wattle/gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
