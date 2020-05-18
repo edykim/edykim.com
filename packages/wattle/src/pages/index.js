b87aa25e0fea7005074c2951032f27d6b96cb5ca
Edward Yong Gyun Kim <edward@edykim.com>
2020-05-18 02:21:56 -0700

    Update English page styles

--- packages/wattle/src/pages/index.js
+++ packages/wattle/src/pages/index.js
@@ -6,42 +6,29 @@ import SEO from "../components/seo"
 const IndexPage = ({ location }) => (
   <Layout location={location}>
     <SEO title="Home" keywords={[`edykim`]} />
-    <h1>
-      Howdy{" "}
+    <blockquote className="hero">Think simple, do more and better.</blockquote>
+    <p>
+      Hey{" "}
       <span role="img" aria-label="wave hand">
         👋
-      </span>
-    </h1>
-    <p>
-      Hey! I'm Edward. I'm passionate about solving complexities and problems
-      through development with a strong background in communication and software
+      </span>{" "}
+      I'm Edward. I'm passionate about solving complexities and problems through
+      development with a strong background in communication and software
       engineering.
     </p>
-
-    <ul>
+    <ul className="main__links">
       <li>
-        <Link to={`/about`}>About Me</Link>
+        <Link to={`/about`}>About</Link>
       </li>
       <li>
-        <a href="https://edykim.com/ko/">한국어 (Korean)</a>
-      </li>
-    </ul>
-
-    <h2>Contact</h2>
-    <ul>
-      <li>
-        <a href="mailto:edward@edykim.com?subject=Hello">edward@edykim.com</a>
+        <Link to={`/archives`}>Posts</Link>
       </li>
       <li>
-        <a href="https://twitter.com/heyedykim">twitter (@heyedykim)</a>
-      </li>
-      <li>
-        <a href="https://github.com/edykim">Github (@edykim)</a>
-      </li>
-      <li>
-        <a href="https://www.linkedin.com/in/edwardykim/">Linkedin</a>
+        <Link to={`/note`}>Notes</Link>
       </li>
     </ul>
+    I have other pages written in Korean,{" "}
+    <a href="https://edykim.com/ko/">check here</a>.
   </Layout>
 )
 
