cd9c8ec0b3947814ad1c71006229c942ab1cb491
Edward Yong Gyun Kim <edward@edykim.com>
2020-09-28 09:03:20 -0700

    Update path of the new micro

--- scripts/new-micro.js
+++ scripts/new-micro.js
@@ -4,13 +4,22 @@ const { writeFileSync, mkdirSync } = require('fs-extra')
 const d = new Date()
 const np = (n) => String(n).padStart(2, '0')
 const y = d.getFullYear()
+const m = d.getMonth() + 1
 
-const stamp = `${y}-${np(d.getMonth() + 1)}-${np(d.getDate())}-${np(
-  d.getHours()
-)}${np(d.getMinutes())}`
+const stamp = `${y}-${np(m)}-${np(d.getDate())}-${np(d.getHours())}${np(
+  d.getMinutes()
+)}`
 
 const filename = `${stamp}.md`
-const p = path.join(__dirname, '..', 'content', 'ko', 'micros', `${y}`)
+const p = path.join(
+  __dirname,
+  '..',
+  'content',
+  'ko',
+  'micros',
+  `${y}`,
+  `${y}-${np(m)}`
+)
 
 mkdirSync(p, { recursive: true })
 writeFileSync(
