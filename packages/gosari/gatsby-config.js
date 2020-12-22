29893c66b89bbe0c02404446862857099480e043
Edward Yong Gyun Kim <edward@edykim.com>
2020-12-22 07:20:58 -0800

    Add katex

--- packages/gosari/gatsby-config.js
+++ packages/gosari/gatsby-config.js
@@ -78,6 +78,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-katex`,
         ],
       },
     },
