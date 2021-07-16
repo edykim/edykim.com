af01d5fd996fa7956cbcc78c0742f2458135b83f
Yong Gyun Kim <edward@edykim.com>
2021-07-16 02:32:43 -0700

    Add new-micro script

---
+++ scripts/new-micro.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const { writeFileSync, mkdirSync } = require('fs')
+
+const d = new Date()
+const np = (n) => String(n).padStart(2, '0')
+const y = d.getFullYear()
+const m = d.getMonth() + 1
+
+const stamp = `${y}-${np(m)}-${np(d.getDate())}-${np(d.getHours())}${np(
+  d.getMinutes()
+)}`
+
+const filename = `${stamp}.md`
+const p = path.join(
+  __dirname,
+  '..',
+  'content',
+  'ko',
+  'micros',
+  `${y}`,
+  `${y}-${np(m)}`
+)
+
+mkdirSync(p, { recursive: true })
+writeFileSync(
+  path.join(p, filename),
+  `---
+# title:
+type: micro
+date: "${d.toISOString()}"
+lang: ko
+slug: "${stamp}"
+---
+
+-
+`
+)
+
+console.log(filename)
