1f3e4c898c8fefa2d574f7240ca59d8d975fbb4e
Edward Kim <33057457+edykim@users.noreply.github.com>
2021-10-28 01:57:08 -0700

    Migrate micros (#63)
    * Migrate micros
    
    * Change new post script

---
+++ scripts/new-ko-post.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const { writeFileSync, mkdirSync } = require('fs')
+
+const categories = require('../config/taxonomy').mappedUrls;
+
+const d = new Date()
+const np = (n) => String(n).padStart(2, '0')
+const y = d.getFullYear()
+const m = d.getMonth() + 1
+
+let title = process.argv.slice(2).join(' ')
+if (title.trim() === '') title = "Untitled";
+
+const slug = title.replace(/\ /gi, '-').toLowerCase()
+
+const stamp = `${y}-${np(m)}-${np(d.getDate())}-${slug}`
+
+const filename = `${stamp}.md`
+const p = path.join(
+  __dirname,
+  '..',
+  'content',
+  'ko',
+  'posts',
+)
+const cat = categories.map(v => `#  - ${v.key}`).join('\n')
+
+mkdirSync(p, { recursive: true })
+writeFileSync(
+  path.join(p, filename),
+  `---
+title: "${title}"
+author: haruair
+type: post
+date: "${d.toISOString()}"
+lang: ko
+# headline:
+#  - sample headline
+categories:
+${cat}
+tags:
+#  - sample-tag
+draft: true
+slug: "${slug}"
+---
+
+-
+`
+)
+
+console.log(filename)
