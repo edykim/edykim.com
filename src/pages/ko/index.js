7b97a920ee375585e3d2ed761d1c4582d734b1ab
Edward Kim <edward@edykim.com>
2022-01-29 16:44:25 -0800

    Update index page

--- src/pages/ko/index.js
+++ src/pages/ko/index.js
@@ -10,11 +10,12 @@ const KoIndexPage = ({ location, data: { featuredItems } }) => {
     <Layout location={location}>
       <Seo title="Home" lang={"ko"} />
       <ContentContainer>
-        <h1>안녕하세요, 김용균입니다.</h1>
+        <h1>안녕하세요, 김용균입니다 👋</h1>
         <p>
-          저는 소프트웨어 엔지니어로 웹서비스와 유용한 도구를 만드는 일을
-          합니다. 작은 것을 만드는 일, 사진 찍는 일, 그리고 맛있는 커피를
-          좋아합니다.
+          저는 웹서비스와 유용한 도구를 만드는 일을 합니다. 작은 도구 만들기를
+          좋아하며 사진과 커피에도 관심이 많습니다. 지금은 미 캘리포니아에서
+          컴퓨터 공부를 하고 있습니다. 아무리 작은 경험이라도 글로 적어두면 나에게도,
+          남에게도 도움이 된다는 생각으로 웹페이지를 꾸리고 있습니다. 🪴
         </p>
         {featuredItems.edges.length > 0 && (
           <PostShortList title={"인기 글"} posts={featuredItems.edges} />
