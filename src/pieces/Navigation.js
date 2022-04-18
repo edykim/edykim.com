1c319b0f6d6437b87260d86262fc8eb50e0a37d7
Edward Kim <edward@edykim.com>
2022-04-17 23:39:01 -0700

    Add partial active on nav

--- src/pieces/Navigation.js
+++ src/pieces/Navigation.js
@@ -2,23 +2,27 @@ import React from "react"
 import { Link } from "gatsby"
 import { usePageLanguage } from "../components/LocationContext"
 
+const NavLink = props => (
+  <Link activeStyle={{ fontWeight: "700" }} partiallyActive={true} {...props} />
+)
+
 const Navigation = () => {
   const language = usePageLanguage()
   if (language === "ko") {
     return (
       <>
-        <Link to="/ko/post/">블로그</Link>
-        <Link to="/ko/notes/">노트</Link>
-        <Link to="/ko/about/">소개</Link>
+        <NavLink to="/ko/post/">블로그</NavLink>
+        <NavLink to="/ko/notes/">노트</NavLink>
+        <NavLink to="/ko/about/">소개</NavLink>
       </>
     )
   }
   return (
     <>
-      <Link to="/crafts/">crafts</Link>
-      <Link to="/writings/">writings</Link>
-      <Link to="/notes/">notes</Link>
-      <Link to="/about/">about</Link>
+      <NavLink to="/crafts/">crafts</NavLink>
+      <NavLink to="/writings/">writings</NavLink>
+      <NavLink to="/notes/">notes</NavLink>
+      <NavLink to="/about/">about</NavLink>
     </>
   )
 }
