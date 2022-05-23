c70c5247bf04f8ef10aa02f47443574d96c01ecb
Edward Kim <edward@edykim.com>
2022-05-23 15:43:40 -0700

    Add tile calendar

--- src/components/content/custom/index.js
+++ src/components/content/custom/index.js
@@ -1,7 +1,14 @@
 import Counter from "./Counter"
 import Grid from "./Grid"
 import CardLink from "./CardLink"
+import TileCalendar, { TileLog } from "./TileCalendar"
 
-const map = { counter: Counter, grid: Grid, "card-link": CardLink }
+const map = {
+  counter: Counter,
+  grid: Grid,
+  "card-link": CardLink,
+  "tile-calendar": TileCalendar,
+  log: TileLog,
+}
 
 export default map
