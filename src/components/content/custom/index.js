20aa0754a8badfce57822988e66098ebcb57f098
Edward Kim <edward@edykim.com>
2022-06-26 12:11:24 -0700

    Add id on header tag

--- src/components/content/custom/index.js
+++ src/components/content/custom/index.js
@@ -2,6 +2,7 @@ import Counter from "./Counter"
 import Grid from "./Grid"
 import CardLink from "./CardLink"
 import TileCalendar, { TileLog } from "./TileCalendar"
+import headerTitles from "./HeaderTitle"
 
 const map = {
   counter: Counter,
@@ -9,6 +10,7 @@ const map = {
   "card-link": CardLink,
   "tile-calendar": TileCalendar,
   log: TileLog,
+  ...headerTitles,
 }
 
 export default map
