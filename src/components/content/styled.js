d3e4c2db4068a3d4d22b7e4dceaa0cc07a9d7e56
Yong Gyun Kim <edward@edykim.com>
2022-12-17 19:33:12 -0800

    Update style

--- src/components/content/styled.js
+++ src/components/content/styled.js
@@ -35,7 +35,7 @@ const Content = styled.div`
   }
   blockquote {
     max-width: var(--site-narrow-max-width);
-    border-left: 5px solid #d7d2cf;
+    border-left: 5px solid var(--color-separator);
     padding-left: 1rem;
     margin-left: var(--site-blockquote-margin);
     margin-right: var(--site-blockquote-margin);
