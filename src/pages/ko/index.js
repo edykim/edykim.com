64354ab629296579b5770477f39164f6b05d9b27
Edward Kim <edward@edykim.com>
2022-12-30 15:54:25 -0800

    Update main page text length

--- src/pages/ko/index.js
+++ src/pages/ko/index.js
@@ -11,10 +11,10 @@ const KoIndexPage = ({ location, data: { featuredItems } }) => {
     <Layout location={location}>
       <Seo title="Home" lang={"ko"} />
       <Content>
-        <p style={{width: '30rem'}}>
+        <p style={{maxWidth: '30rem'}}>
           안녕하세요, <strong>김용균</strong>입니다. 🪴
         </p>
-        <p style={{width: '30rem'}}>
+        <p style={{maxWidth: '30rem'}}>
           저는 일상에서 유용하게 사용할 수 있는 디지털 도구와 웹서비스를 만드는 일을 합니다.
           이 공간에는 번역, 개발 관련 글을 주로 게시하며 일상 주제로도 자주 작성하고 있습니다.
         </p>
