6c61763008e390eb41eb4ba9135f0c5d0501ab17
Edward Kim <edward@edykim.com>
2023-07-09 11:54:57 -0700

    Add menu toggle button

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -30,6 +30,19 @@ const SideContainer = styled.aside`
     color: var(--color-title);
     text-decoration: none;
   }
+  &.full-menu {
+    @media screen and (max-width: 999px) {
+      position: fixed;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      background-color: var(--color-background);
+      display: block !important;
+      z-index: 1000;
+      overflow-y: auto;
+      font-size: 1.1rem;
+    }
+  }
 `
 
 function Node({ node, isTag = false }) {
@@ -60,32 +73,34 @@ function Node({ node, isTag = false }) {
   )
 }
 
-export default function Sidebar() {
+export default function Sidebar({forceShow}) {
   return (
-    <SideContainer>
-      <ul>
-        <Node
-          node={{
-            key: `블로그`,
-            url: `post`,
-          }}
-        />
-        <Node
-          node={{
-            key: `아카이브`,
-            url: `archives`,
-          }}
-        />
-        <Node
-          node={{
-            key: `소개`,
-            url: `about`,
-          }}
-        />
-        {taxonomy.tree.children.map(c => (
-          <Node key={`side-link-${c.key}`} node={c} isTag={true} />
-        ))}
-      </ul>
-    </SideContainer>
+    <>
+      <SideContainer className={forceShow ? 'full-menu' : null}>
+        <ul>
+          <Node
+            node={{
+              key: `블로그`,
+              url: `post`,
+            }}
+          />
+          <Node
+            node={{
+              key: `아카이브`,
+              url: `archives`,
+            }}
+          />
+          <Node
+            node={{
+              key: `소개`,
+              url: `about`,
+            }}
+          />
+          {taxonomy.tree.children.map(c => (
+            <Node key={`side-link-${c.key}`} node={c} isTag={true} />
+          ))}
+        </ul>
+      </SideContainer>
+    </>
   )
 }
