d7d92c04b26935e281971dece8c8387e3c1e779c
Edward Kim <edward@edykim.com>
2024-02-07 18:52:48 -0800

    Remove comment

--- src/components/comment.js
+++ src/components/comment.js
@@ -14,34 +14,36 @@ function assignAttrs(el, attrs) {
   }
 }
 
-const Comment = () => {
-  const ref = useRef(null)
-  useEffect(() => {
-    const se = document.createElement("script")
-    assignAttrs(se, {
-      src: "https://giscus.app/client.js",
-      "data-repo":"edykim/edykim.com",
-      "data-repo-id":"MDEwOlJlcG9zaXRvcnkxMDgxMTYwNDY=",
-      "data-category":"Announcements",
-      "data-category-id":"DIC_kwDOBnG4Ts4CTVMX",
-      "data-mapping":"pathname",
-      "data-strict":"0",
-      "data-reactions-enabled":"0",
-      "data-emit-metadata":"0",
-      "data-input-position":"bottom",
-      "data-theme":"https://edykim.com/giscus.css",
-      "data-lang":"en",
-      "data-loading":"lazy",
-      "crossorigin":"anonymous",
-      async: true,
-    })
-    ref.current.appendChild(se)
-  }, [])
-  return (
-    <CommentContainer>
-      <div ref={ref}></div>
-    </CommentContainer>
-  )
-}
+const Comment = () => null
+
+// const Comment = () => {
+//   const ref = useRef(null)
+//   useEffect(() => {
+//     const se = document.createElement("script")
+//     assignAttrs(se, {
+//       src: "https://giscus.app/client.js",
+//       "data-repo":"edykim/edykim.com",
+//       "data-repo-id":"MDEwOlJlcG9zaXRvcnkxMDgxMTYwNDY=",
+//       "data-category":"Announcements",
+//       "data-category-id":"DIC_kwDOBnG4Ts4CTVMX",
+//       "data-mapping":"pathname",
+//       "data-strict":"0",
+//       "data-reactions-enabled":"0",
+//       "data-emit-metadata":"0",
+//       "data-input-position":"bottom",
+//       "data-theme":"https://edykim.com/giscus.css",
+//       "data-lang":"en",
+//       "data-loading":"lazy",
+//       "crossorigin":"anonymous",
+//       async: true,
+//     })
+//     ref.current.appendChild(se)
+//   }, [])
+//   return (
+//     <CommentContainer>
+//       <div ref={ref}></div>
+//     </CommentContainer>
+//   )
+// }
 
 export default Comment
